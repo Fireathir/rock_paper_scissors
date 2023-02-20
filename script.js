b2c0f020528bd812397ae2b3d7e06bb78f8baacb
Jimmy Brandt <jimmy_brandt@hotmail.com>
2023-02-20 14:07:37 +0100

    .

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 // Rock, paper, scissors
 let userChoice;
 let computerChoice;
+let comparisonResult = "string";
 
 //function to collect the users input
-function userInput(userchoice){
+function userInput(userChoice){
     userChoice = prompt("Choose rock, paper or scissors!");
-    return userChoice;
+    return userChoice.toLowerCase();
 }
 
 //random generator for computer choice
@@ -28,9 +29,45 @@ function computerInput(computerChoice){
 }
 
 
-// function to run the game
+//function to compare the results
+function compareResults(){
+    if (userChoice == "rock" && computerChoice == "rock"){
+        return comparisonResult = "It's a draw! You both picked the same option!";
+    } 
+    else if (userChoice == "rock" && computerChoice == "paper"){
+        return comparisonResult = "You lose! You chose rock, the computer chose paper!";
+    } 
+    else if (userChoice == "rock" && computerChoice == "scissors"){
+        return comparisonResult = "You win! You chose rock, the computer chose scissors!";
+    } 
+    else if (userChoice == "paper" && computerChoice == "paper"){
+        return comparisonResult = "It's a draw! You both picked the same option!";
+    }
+    else if (userChoice == "paper" && computerChoice == "rock"){
+        return comparisonResult = "You win! You chose paper, the computer chose rock!";
+    } 
+    else if (userChoice == "paper" && computerChoice == "scissors"){
+        return comparisonResult = "You lose! You chose paper, the computer chose Scissors!";
+    } 
+    else if (userChoice == "scissors" && computerChoice == "scissors"){
+        return comparisonResult = "It's a draw! You both picked the same option!";
+    }
+    else if (userChoice == "scissors" && computerChoice == "paper"){
+        return comparisonResult = "You win! You chose scissors, the computer chose paper!";
+    } 
+    else if (userChoice == "scissors" && computerChoice == "rock"){
+        return comparisonResult = "You lose! You chose scissors, the computer chose rock!";
+    }
+}
+//Game function
+function game(){
+    userChoice = userInput();
+    computerChoice = computerInput();
+    comparisonResult = (compareResults());
+    console.log(comparisonResult);
+}
 
-console.log(userInput());
-console.log(randomNumber(3));
-console.log(computerInput());
-console.log(userChoice);
+
+
+//Run the game
+game();
