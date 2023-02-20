c4e81cb296e745e0b06452edccd24d99aed0380b
Jimmy Brandt <jimmy_brandt@hotmail.com>
2023-02-20 13:24:34 +0100

    .

--- script.js
+++ script.js
@@ -1,49 +1,36 @@
+// Rock, paper, scissors
+let userChoice;
 let computerChoice;
-let result;
-let playerChoice = prompt("Rock, paper or scissor?")
-playerChoice = playerChoice.toLowerCase();
-let playerCounter = 0;
-let computerCounter =0;
 
-function getComputerChoice (){
-    const randomNumber = Math.floor(Math.random()*3 +1);
-    if (randomNumber === 1){
+//function to collect the users input
+function userInput(userchoice){
+    userChoice = prompt("Choose rock, paper or scissors!");
+    return userChoice;
+}
+
+//random generator for computer choice
+function randomNumber(number){
+    return Math.floor(Math.random() * number);
+}
+    
+//funtion to generat the computers choice with the random number generator as base
+function computerInput(computerChoice){
+    let x = randomNumber(2);
+    if (x == 0){
         return computerChoice = "rock";
     }
-    else if (randomNumber === 2){
-        return computerChoice = "scissor";
-    }
-    else if (randomNumber === 3){
+    else if (x == 1){
         return computerChoice = "paper";
     }
+    else if (x == 2){
+        return computerChoice = "scissors";
+    }
 }
 
-function gameRound (playerChoice, computerChoice){
-    if (playerChoice == "rock" && computerChoice == "rock"){
-        result ="You draw!";
-    }
-    else if (playerChoice =="rock" && computerChoice == "scissor"){
-        result = "You win!";
-    }
-    else if (playerChoice == "rock" && computerChoice=="paper"){
-        result ="You lose!";
-    }
-    else if (playerChoice == "paper" && computerChoice =="scissor"){
-        result ="You lose!";
-    }
-    else if (playerChoice =="paper" && computerChoice == "rock"){
-        result = "You win!";
-    }
-    else if (playerChoice == "scissor" && computerChoice == "rock"){
-        result = "You Lose!";
-    }
-    else if (playerChoice == "scissor" && computerChoice == "paper"){
-        result = "You Win!"
-    }
-    return result;
-}
 
-getComputerChoice();
-gameRound();
-console.log(computerChoice);
-console.log(result);
+// function to run the game
+
+console.log(userInput());
+console.log(randomNumber(3));
+console.log(computerInput());
+console.log(userChoice);
